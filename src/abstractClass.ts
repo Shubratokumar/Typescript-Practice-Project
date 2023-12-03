5db6c2b999cf1c13cead8304608e88919924005a
Shubrato Kumar <shubratokumargharami920@gmail.com>
2023-12-03 12:20:58 +0600

    more about abstruct class and super method in typescript

--- src/abstractClass.ts
+++ src/abstractClass.ts
@@ -4,8 +4,9 @@ abstract class TakePhoto {
         public cameraMode: string,
         public filter: string
     ){}
-
+    // use abstract in case of compulsory
     abstract getSepia(): void
+    // optional
     getReelCount(): number{
         // here are some complex calculations
         return 5
@@ -16,8 +17,11 @@ class Instagram1 extends TakePhoto {
         public cameraMode: string,
         public filter: string,
         public burst: number,        
-    ){
+    ){        
+        // console.log(this.cameraMode);  this is wrong
+        // 'super' must be called before accessing 'this' in the constructor of a derived class.
         super(cameraMode, filter)
+        // console.log(this.cameraMode); this is completely ok 
     }
     getSepia(): void {
         console.log("Sepia is here");
